6d6054ca8f384bd863760537403853891650d365
Eric Skala <eric.skala@telekom.de>
2015-04-14 09:59:25 +0200

    tar CentOS 6 files, since it is deprecated

--- CentOS-6-x86_64/scripts/network.sh
+++
@@ -1,5 +0,0 @@
-# echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-# echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-# echo 'NM_CONTORLLED="no"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
-# systemctl disable iptables
-# systemctl disable firewalld
