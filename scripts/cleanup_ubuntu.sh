9e6874955bd737d9a7363977631ed4ced570be9c
Eric Skala <e.skala@telekom.de>
2015-05-10 02:22:34 +0200

    major change in build.sh, 90% rewritten

--- scripts/cleanup_ubuntu.sh
+++ scripts/cleanup_ubuntu.sh
@@ -1,5 +1,5 @@
 # Clean up unneeded packages.
-apt-get -y purge apparmor dictionaries-common eject friendly-recovery ftp fuse gdisk hdparm landscape-common language-pack-gnome-en laptop-detect libicu52:amd64 linux-headers-3.16.0-30 linux-image-extra-3.16.0-30-generic python3:i386 python3.4:i386 python3-minimal:i386 python3.4-minimal:i386 telnet tmux wamerican wbritish wireless-tools
+apt-get -y purge apparmor dictionaries-common eject friendly-recovery ftp fuse gdisk hdparm landscape-common language-pack-gnome-en laptop-detect libicu52:amd64 linux-headers-3.16.0-37 linux-image-extra-3.16.0-37-generic telnet tmux wamerican wbritish wireless-tools ufw whiptail libx11-6 libx11-data libxdmcp6 libxau6 xkb-data
 apt-get -y autoremove
 apt-get -y clean
 > /root/.ssh/authorized_keys
