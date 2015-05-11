c829e3f0f09b3ce33fa0a3a1073c0495b402e568
Eric Skala <e.skala@telekom.de>
2015-05-11 10:48:22 +0200

    removed travis CI, wrong order...

--- scripts/cleanup_ubuntu.sh
+++ scripts/cleanup_ubuntu.sh
@@ -1,5 +1,6 @@
 # Clean up unneeded packages.
-apt-get -y purge apparmor dictionaries-common eject friendly-recovery ftp fuse gdisk hdparm landscape-common language-pack-gnome-en laptop-detect libicu52:amd64 linux-headers-3.16.0-37 linux-image-extra-3.16.0-37-generic telnet tmux wamerican wbritish wireless-tools ufw whiptail libx11-6 libx11-data libxdmcp6 libxau6 xkb-data
+apt-get -y purge apparmor dictionaries-common eject friendly-recovery ftp fuse gdisk hdparm landscape-common language-pack-gnome-en laptop-detect libicu52:amd64 telnet tmux wamerican wbritish wireless-tools ufw whiptail libx11-6 libx11-data libxdmcp6 libxau6 xkb-data
+dpkg -l linux-{image,headers}-* | awk '/^ii/{print $2}' | egrep '[0-9]+\.[0-9]+\.[0-9]+' | grep -v $(uname -r) | xargs sudo apt-get -y purge
 apt-get -y autoremove
 apt-get -y clean
 > /root/.ssh/authorized_keys
