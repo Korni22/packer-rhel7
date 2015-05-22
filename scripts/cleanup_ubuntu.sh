560c22140dde9f28621212be8718d1af74b49cd3
Eric Skala <e.skala@telekom.de>
2015-05-22 11:06:50 +0200

    Change cleanup script for ubuntu, since it uninstalled cloud-init

--- scripts/cleanup_ubuntu.sh
+++ scripts/cleanup_ubuntu.sh
@@ -1,7 +1,8 @@
 # Clean up unneeded packages.
-apt-get -y purge accountsservice apparmor console-setup dosfstools eject fuse geoip-database gir1.2-glib-2.0 hdparm keyboard-configuration krbb5-locales language-pack-de laptop-detect parted powermgmt-base python telnet xauth xkb-data
-dpkg -l linux-{image,headers}-* | awk '/^ii/{print $2}' | egrep '[0-9]+\.[0-9]+\.[0-9]+' | grep -v $(uname -r) | xargs sudo apt-get -y purge
+apt-get -y purge accountsservice apparmor console-setup dosfstools eject fuse geoip-database hdparm keyboard-configuration krb5-locales language-pack-de laptop-detect parted powermgmt-base telnet xauth xkb-data  > /dev/null && echo "Packages cleaned up!" || echo "wat"
+dpkg -l linux-{image,headers}-* | awk '/^ii/{print $2}' | egrep '[0-9]+\.[0-9]+\.[0-9]+' | grep -v $(uname -r) | xargs sudo apt-get -y purge > /dev/null && echo "Unused Kernel-shit removed!" || echo "wat2"
 apt-get -y autoremove
 apt-get -y clean
+rm -rf /var/lib/apt/lists/*
 > /root/.ssh/authorized_keys
 shutdown -h now
